73c222ba70df76476386080704c2dea4caa5a0a8
Med10S <mohammedsbihi11@gmail.com>
2025-08-10 13:16:55 +0100

    clean the code for the suricata server branch

--- wazuh-docker/single-node/ManagerConfig/active-response/enable-network.cmd
+++
@@ -1,6 +0,0 @@
-:: C:\Program Files (x86)\ossec-agent\active-response\bin\enable-network.cmd
-@echo off
-netsh advfirewall set allprofiles firewallpolicy allowinbound,allowoutbound
-netsh advfirewall firewall delete rule name="AllowManager"
-netsh advfirewall firewall delete rule name="AllowManagerIn"
-eventcreate /T INFORMATION /ID 1001 /L APPLICATION /D "OSSEC: Network enabled (restored)"
--- wazuh-docker/single-node/ManagerConfig/active-response/enable-network.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Restore default networking
-iptables -F
-iptables -P INPUT ACCEPT
-iptables -P OUTPUT ACCEPT
-
-logger "OSSEC: Network enabled (restored)"
-exit 0
